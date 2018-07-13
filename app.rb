808dccd76d1dbc49946442387766db0fd2e82492
ERIC KELLY <eakelly220@gmail.com>
2018-07-13 01:16:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,10 +5,13 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
+<<<<<<< HEAD
 
   post '/piglatinize' do
     pig = PigLatinizer.new
     @pigtext = pig.piglatinize(params[:user_phrase])
     erb :piglatinize
   end
+=======
+>>>>>>> 698407331953f95848b8f6620e91e296637fb535
 end
